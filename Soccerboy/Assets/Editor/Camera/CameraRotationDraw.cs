23b7be2fae99abf6c30309240fffd06f2b0e9e48
valentinmelchor <valentinmelchor1401@gmail.com>
2018-01-18 14:05:02 -0300

    Camara rotatoria 90°
    La camara solo esta aplicada en la escena "CameraRotation_Test", se debe apretar la Q para rotar

---
+++ Soccerboy/Assets/Editor/Camera/CameraRotationDraw.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(RotateCamera))]
+class ConnectLineHandleExampleScript : Editor
+{
+    void OnSceneGUI()
+    {
+        RotateCamera rotateCamera = target as RotateCamera;
+        Handles.color = Color.blue;
+        Handles.DrawWireArc(rotateCamera.transform.position, rotateCamera.transform.up, Camera.main.transform.localPosition, rotateCamera.changeAngle, Vector3.Distance(rotateCamera.transform.position, Camera.main.transform.position));
+    }
+}
---
+++ Soccerboy/Assets/Scripts/Camera/RotateCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour {
+    private int angle;
+    public int changeAngle;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            angle = angle == 0 ? changeAngle : 0;
+        }
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, angle, 0), 10 * Time.deltaTime);
+    }
+}
