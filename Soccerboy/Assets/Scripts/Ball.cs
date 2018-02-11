ad93074a874c331b34efa8e170673198c0a9f537
David Rochin <jdrc8@hotmail.com>
2018-02-10 22:07:25 -0700

    Cambios importantes a las fiscas de la pelota
    Se cambió la manera en la que la pelota se mueve por las pendientes totalmente. El cambio aún no está terminado; falta hacer que, entre mas inclinada esté la pendiente, más rapido baja por ella.

--- Soccerboy/Assets/Scripts/Ball.cs
+++ Soccerboy/Assets/Scripts/Ball.cs
@@ -10,10 +10,12 @@ public class Ball : MonoBehaviour {
     public LayerMask floorLayerMask;
     public LayerMask wallLayerMask;
 
-    [HideInInspector] public Vector3 bottom {
+    [HideInInspector]
+    public Vector3 bottom {
         get { return transform.position + Vector3.down * sphereCollider.radius; }
         set { transform.position = value + Vector3.up * sphereCollider.radius; }
     }
+    [HideInInspector] public bool launched;
 
     SphereCollider sphereCollider;
 
@@ -22,64 +24,50 @@ public class Ball : MonoBehaviour {
     //Eventos
     public event Action OnOutOfField;
 
-	void Awake () {
+    void Awake() {
         sphereCollider = GetComponent<SphereCollider>();
 
         previousPosition = transform.position;
 
         hitInfos = new List<HitInfo>();
-	}
-	
-	void Update () {
+    }
+
+    void Update() {
 
         //Limitar la velocidad
         velocity = Vector3.ClampMagnitude(velocity, 20f);
 
-        //Recopilar datos del suelo
-        //RaycastHit hit; bool thereIsFloor = Physics.Raycast(transform.position + Vector3.up * sphereCollider.radius, Vector3.down, out hit, 10f, floorLayerMask);
-        RaycastHit sphereHit; bool thereIsFloor = Physics.SphereCast(transform.position + Vector3.up * sphereCollider.radius * 2f, sphereCollider.radius, Vector3.down, out sphereHit, sphereCollider.radius * 2f, floorLayerMask);
-
         //Revisar si la pelota se cayó del campo
-        if(transform.position.y < -1f) {
+        if (transform.position.y < -1f) {
             if (OnOutOfField != null) { OnOutOfField(); }
         }
 
-        //Si no hay suelo, caer
-        if (thereIsFloor == false) {
-            Debug.Log("No hay suelo");
-            //Caer
-            velocity = velocity + Vector3.down * Time.deltaTime * 16f;
-        } 
-        
-        //Si hay suelo
-        else {
+        //Disminuir la velocidad
+        Deacelerate(2f);
 
-            //Disminuir la velocidad
-            Deacelerate(2f);
-
-            //Pegarse a el
-            //transform.position = new Vector3(transform.position.x, hit.point.y + sphereCollider.radius, transform.position.z);
-            
-            //Si el suelo está mas arriba que la base de la pelota, pegarse a el
-            if(sphereHit.point.y > transform.position.y - sphereCollider.radius) {
-                Debug.Log("Suelo mas arriba");
-                transform.position = transform.position + Vector3.up * sphereCollider.radius * 2f + Vector3.down * sphereHit.distance;
-            } else if(Mathf.Approximately(sphereHit.point.y, transform.position.y - sphereCollider.radius)) {
-                Debug.Log("Suelo igual");
-            } else {
-                Debug.Log("Suelo abajo");
-                //Caer
-                velocity = velocity + Vector3.down * Time.deltaTime * 16f;
-            }
-
-            //Darle velocidad de acuerdo a la pendiente
-            Vector3 velAdd = Vector3Util.NoY(sphereHit.normal);
-            //Vector3 velAdd = Vector3Util.NoY(hit.normal);
-            velocity = velocity + velAdd * Time.deltaTime * 16f;
-        }
+        //Caer
+        Fall(16f);
 
         //Moverse de acuerdo a la velocidad
         transform.position = transform.position + velocity * Time.deltaTime;
+
+        //Recopilar datos del suelo
+        RaycastHit sphereHit; Vector3 castStart = transform.position + Vector3.up * sphereCollider.radius * 2f;
+        bool thereIsFloor = Physics.SphereCast(castStart, sphereCollider.radius, Vector3.down, out sphereHit, sphereCollider.radius * 2f, floorLayerMask);
+
+        //Si hay suelo
+        if (thereIsFloor) {
+
+            //Quitar la velocidad en Y
+            velocity = new Vector3(velocity.x, 0f, velocity.z);
+
+            //Darle velocidad de acuerdo a la pendiente
+            Vector3 velAdd = Vector3.Cross(sphereHit.normal, Quaternion.Euler(0f, -90f, 0f) * sphereHit.normal.NoY());
+            velocity = velocity + velAdd * Time.deltaTime * 16f;
+
+            transform.position = castStart + Vector3.down * sphereHit.distance;
+
+        }
 
         //Manejar colisión
         ManageCollision();
@@ -123,12 +111,21 @@ public class Ball : MonoBehaviour {
         velocity = velocity.normalized * newMagnitude;
     }
 
+    public void Fall(float factor) {
+        velocity = velocity + Vector3.down * Time.deltaTime * factor;
+    }
+
+    Vector3 pendNorm;
+    Vector3 pendPos;
+
     private void OnDrawGizmos() {
         //Dibujar los HitInfo
         Gizmos.color = Color.red;
         foreach (HitInfo i in hitInfos) {
             Gizmos.DrawRay(i.point, i.normal);
         }
+
+        Gizmos.DrawRay(pendPos, pendNorm);
     }
 
     public static float ballRadius = 0.4457389f;
--- Soccerboy/Assets/Scripts/Util/ExtensionMethods.cs
+++ Soccerboy/Assets/Scripts/Util/ExtensionMethods.cs
@@ -7,4 +7,8 @@ public static class ExtensionMethods {
     public static Vector3 NoY(this Vector3 vector) {
         return new Vector3(vector.x, 0f, vector.z);
     }
+
+    public static Vector3 InvertedY(this Vector3 vector) {
+        return new Vector3(vector.x, -vector.y, vector.z);
+    }
 }
