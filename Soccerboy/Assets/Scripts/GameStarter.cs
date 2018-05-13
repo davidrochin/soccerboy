342b4c790a7537b95c371b53930709340b7d816f
David Rochin <jdrc8@hotmail.com>
2018-05-12 21:31:46 -0600

    Inicios de una pantalla de inicio
    -Se agregó y acomodó un poco de arte en la pantalla de carga inicial.

--- Soccerboy/Assets/Scripts/GameStarter.cs
+++ Soccerboy/Assets/Scripts/GameStarter.cs
@@ -11,12 +11,26 @@ using UnityEngine;
 
 public class GameStarter : MonoBehaviour {
 
-	void Start () {
+	void Awake () {
+        
+        //Si el jugador es nuevo, solicitar la creacion de un nuevo perfil y guardar su id y su pass
+        if(PlayerPrefs.GetString("local_player_id", "null").Equals("null")) {
+
+        }
+
+        //Si no es nuevo, solicitar los datos del jugador al servidor
+        else {
+            RequestPlayerData();
+        }
 
         //Obtener el jugador con ID 2, desde el servidor, como prueba.
-        DataRequester.GetPlayer(2, delegate(Player p) {
+        /*DataRequester.GetPlayer(2, delegate(Player p) {
             Debug.Log(p);    
-        });
+        });*/
 	}
+
+    void RequestPlayerData() {
+
+    }
 	
 }
